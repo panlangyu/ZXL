a2436dc9a47590e3f47bd63a38034c1033418ae6
panlangyu <panlangyuq@163.com>
2018-09-06 18:59:25 +0800

    增加异常信息

--- src/main/java/pro/bechat/wallet/domain/exception/RestExceptionHandler.java
+++ src/main/java/pro/bechat/wallet/domain/exception/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package pro.bechat.wallet.domain.exception;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.TypeMismatchException;
@@ -8,7 +9,10 @@ import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import pro.bechat.wallet.domain.model.response.ApiResponseResult;
 import java.io.IOException;
@@ -17,17 +21,38 @@ import java.sql.SQLException;
 /**
  * 系统异常的统一处理
  */
+@RestController
+@ControllerAdvice
 public class RestExceptionHandler {
 
 
     private final static Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
+
+    //运行参数数值转换
+    @ExceptionHandler(value=NumberFormatException.class)
+    public ApiResponseResult numberFormatException(NumberFormatException ex){
+
+        this.logger.error("【服务器运行数值转换异常】 {} : "+ ex.getMessage());
+        ex.printStackTrace();
+        return ApiResponseResult.build(500,"error","[服务器运行数值转换异常]："+ex.getMessage(),"");
+    }
+
+    //参数数值转换
+    @ExceptionHandler(value=MethodArgumentTypeMismatchException.class)
+    public ApiResponseResult methodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex){
+
+        this.logger.error("【服务器参数数值转换异常】 {} : "+ ex.getMessage());
+        ex.printStackTrace();
+        return ApiResponseResult.build(500,"error","[服务器参数数值转换异常]：Failed to convert value of type 'java.lang.String' to required type 'java.lang.Integer'","");
+    }
 
     //RSA加密异常
     @ExceptionHandler(value=ArrayIndexOutOfBoundsException.class)
     public ApiResponseResult arrayIndexOutOfBoundsException(ArrayIndexOutOfBoundsException ex) {
 
         this.logger.error("【服务器加密算法异常】 {} : "+ ex.getMessage());
-        //ex.printStackTrace();
+        ex.printStackTrace();
         return ApiResponseResult.build(1012,"error","[服务器加密算法异常]：too much data for RSA block","");
     }
 
